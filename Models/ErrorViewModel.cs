65988db71b92a625ce42cdd2fe251db11f8af8c9
adrinetor86 <adrinetor81@gmail.com>
2026-06-03 09:26:48 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AwsZapasAdrianJacek.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
